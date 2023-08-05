e368f206e9243106239e4a4beb0eaba2a947c9d0
aripandini <ariannepandini@hotmail.com>
2023-08-05 14:03:07 -0400

    database set for merge

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,16 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+//  Accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: id, jade: content });
   const result = await request;
   console.log('Data saved to the database', result);
 }
-// TODO: Add logic for a method that gets all the content from the database
+// Gets all the content from the database
 export const getDb = async () => {
  const jateDb = await openDB('jate', 1);
  const tx = jateDb.transaction('jate', 'readonly');
