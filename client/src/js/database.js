8e6cedd15a5c12ec2fa8d23df909bd4fe94c577d
aripandini <ariannepandini@hotmail.com>
2023-08-08 10:32:26 -0400

    added some comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,21 +14,33 @@ const initdb = async () =>
 
 //  Accepts some content and adds it to the database
 export const putDb = async (content) => {
+  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store.
   const store = tx.objectStore('jate');
+  // Use the .put() method on the store and pass in the content.
+  //We're storing everything under same id
   const request = store.put({ id: 1, jade: content });
+  // Get confirmation of the request.
   const result = await request;
   console.log('Data saved to the database', result);
 }
 // Gets all the content from the database
 export const getDb = async () => {
+  // Create a connection to the database database and version we want to use.
  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
  const tx = jateDb.transaction('jate', 'readonly');
+ // Open up the desired object store.
  const store = tx.objectStore('jate');
+ // Use the .getAll() method to get all data in the database.
  const request = store.getAll();
+ // Get confirmation of the request.
  const result = await request;
 
+ //Making friends with code mirror, it likes the results this way
  if ( result.length > 0 ) {
   return result[0].jade
  } else {
